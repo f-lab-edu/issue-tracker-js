861e866e80ef1a8ecab20fcb647b4f0c568c1eb4
bytrustu <bytrustu@gmail.com>
2023-03-26 13:50:08 +0900

    feat: axios 설정 및 테스트 API 추가

---
+++ src/lib/api/config.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+const request = axios.create({
+  baseURL: '/api',
+  timeout: 3000,
+});
+
+request.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+request.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(new CustomError(error.message));
+  },
+);
+
+export class CustomError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'CustomError';
+  }
+}
+
+export const handleErrorResponse = (error) => {
+  if (error instanceof CustomError) {
+    console.log('CustomError: ', error.name, error.message);
+    return;
+  }
+  console.log('Error: ', error?.message);
+};
+
+export default request;
