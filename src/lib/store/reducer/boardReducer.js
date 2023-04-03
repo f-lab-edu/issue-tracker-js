1060a10db833588885cf08f88226767fc49c5080
bytrustu <bytrustu@gmail.com>
2023-04-04 02:12:42 +0900

    feat: BOARD Reducer 기능 추가

--- src/lib/store/reducer/boardReducer.js
+++ src/lib/store/reducer/boardReducer.js
@@ -1,29 +1,85 @@
-import { getUniqueId } from '../../utils/string';
-
-const makeBoard = (title = '') => ({
-  id: getUniqueId(),
-  title,
-  items: [],
-});
-
-const ADD_BOARD_COLUMN_ACTION = 'BOARD/ADD_COLUMN';
+export const SET_INITIAL_BOARD_ACTION = 'BOARD/SET_INITIAL';
+export const ADD_BOARD_COLUMN_ACTION = 'BOARD/ADD_COLUMN';
+export const ADD_BOARD_ITEM_ACTION = 'BOARD/ADD_ITEM';
+export const OPEN_BOARD_TEXTAREA_ACTION = 'BOARD/OPEN_TEXTAREA';
+export const CLOSE_BOARD_TEXTAREA_ACTION = 'BOARD/CLOSE_TEXTAREA';
 
 export const initialState = {
-  boards: [makeBoard('할 일'), makeBoard('진행 중'), makeBoard('완료')],
+  boards: [],
 };
 
-export const addBoardAction = (title) => ({
-  type: ADD_BOARD_COLUMN_ACTION,
-  payload: title,
+export const setInitialBoardAction = (payload) => ({
+  type: SET_INITIAL_BOARD_ACTION,
+  payload,
+});
+
+export const addBoardItemAction = (payload) => ({
+  type: ADD_BOARD_ITEM_ACTION,
+  payload,
+});
+
+export const openBoardTextareaAction = (payload) => ({
+  type: OPEN_BOARD_TEXTAREA_ACTION,
+  payload,
+});
+
+export const closeBoardTextareaAction = (payload) => ({
+  type: CLOSE_BOARD_TEXTAREA_ACTION,
+  payload,
 });
 
 const boardReducer = (state, action) => {
   switch (action.type) {
+    case SET_INITIAL_BOARD_ACTION:
+      return {
+        ...state,
+        boards: action.payload,
+      };
     case ADD_BOARD_COLUMN_ACTION:
       return {
         ...state,
-        boards: [...state.boards, makeBoard(action.payload)],
+        boards: [],
       };
+    case ADD_BOARD_ITEM_ACTION:
+      return {
+        ...state,
+        boards: state.boards.map((board) => {
+          if (board.id === action.payload.id) {
+            return {
+              ...board,
+              items: [...board.items, action.payload.item],
+            };
+          }
+          return board;
+        }),
+      };
+    case OPEN_BOARD_TEXTAREA_ACTION:
+      return {
+        ...state,
+        boards: state.boards.map((board) => {
+          if (board.id === action.payload) {
+            return {
+              ...board,
+              isTextareaOpen: true,
+            };
+          }
+          return board;
+        }),
+      };
+    case CLOSE_BOARD_TEXTAREA_ACTION:
+      return {
+        ...state,
+        boards: state.boards.map((board) => {
+          if (board.id === action.payload) {
+            return {
+              ...board,
+              isTextareaOpen: false,
+            };
+          }
+          return board;
+        }),
+      };
+
     default:
       return state;
   }
--- src/lib/utils/string.js
+++ src/lib/utils/string.js
@@ -1,1 +1,7 @@
-export const getUniqueId = () => new Date().getTime().toString(36) + Math.random().toString(36).slice(2);
+export const getUniqueId = (length) => {
+  const uniqueId = new Date().getTime().toString(36) + Math.random().toString(36).slice(2);
+  if (length) {
+    return uniqueId.slice(uniqueId.length - length, uniqueId.length);
+  }
+  return uniqueId;
+};
